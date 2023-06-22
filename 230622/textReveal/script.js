612eda297d7630f38aa9c3d7c3b314cee53a72ff
ingkejin <ingkejin@gmail.com>
2023-06-22 14:18:46 +0900

    mouse 이벤트 중간과정

---
+++ 230622/textReveal/script.js
@@ -0,0 +1,37 @@
+const title = document.getElementById('title')
+// console.log(title)
+// console.log(title.textContent)
+// console.log(title.innerText)
+// 매개변수 -> 구분자
+// title.addEventListener('click',() => {
+//   title.classList.toggle('active')
+// })
+const chars = title.innerText.split('')
+
+// 1. html에 있는 title 안의 문자열 삭제
+title.innerText = '';
+
+// 2. span태그에 담아서 title안에 
+// 자바스크립트로 생성한 문자열 삽입
+chars.forEach((char, index) => {
+  //  span element  생성
+  const span = document.createElement('span')
+  // class 'char' 추가
+  span.classList.add('char')
+  // 생성된 span 태그에 char 문자 삽입
+  span.textContent = char
+  // title에 자식요소로 문자가 담긴 span 태그 삽입
+  title.appendChild(span)
+  // console.log(span)
+
+  setTimeout(() => {
+    span.classList.add('active')
+  }, 200 * index)
+})
+
+// setTimeout은 순차적으로 실행되는 것이 아니라
+// 자신이 갖고 있는 timer에 맞게 실행 타이밍을 가져간다.
+// 비동기 함수
+setTimeout(() => {console.log('첫번째 setTimeout')}, 500)
+setTimeout(() => {console.log('두번째 setTimeout')}, 800)
+setTimeout(() => {console.log('세번째 setTimeout')}, 100)
