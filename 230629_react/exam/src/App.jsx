7301d1f051ee5472c8413d3931c146321071cb9a
ingkejin <ingkejin@gmail.com>
2023-06-29 16:20:53 +0900

    버튼 컴포넌트

--- 230629_react/exam/src/App.jsx
+++ 230629_react/exam/src/App.jsx
@@ -1,19 +1,22 @@
 import './App.css'
-// size 속성을 만들어서
-// font-size도 매개변수로 받아 적용해보세요
-function Button({name, color, size}){
-  return(
-    <button style={{color:color,fontSize:size}}>{name}</button>
-  )
-}
+import Button from './components/Button.jsx'
+
+// components/Heading.jsx
+// export default function Heading()
+// h1 태그
+// title 속성을 받아서 화면에 렌더링
+// import Heading from './components/Heading.jsx'
 
 function App() { 
   return (    
     <>      
-      <h1>안녕하세요</h1>  
-      <p>리액트 입문</p>   
+      <Heading title="안녕하세요"/>
       <Button name="1번버튼" color="red" size="16px"/>
       <Button name="2번버튼" color="green" size="18px"/>
+      <Button name="3번버튼" color="gray" size="24px"/>
+      <Button name="3번버튼" color="gray" size="24px"/>
+      <Button name="3번버튼" color="gray" size="24px"/>
+      <Button name="3번버튼" color="gray" size="24px"/>
       <Button name="3번버튼" color="gray" size="24px"/>
     </>
   )
