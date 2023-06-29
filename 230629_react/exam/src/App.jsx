9d46c1d5ea51ffdcc07a7097b4d997e422c5f031
ingkejin <ingkejin@gmail.com>
2023-06-29 16:13:42 +0900

    컴포넌트 작성

--- 230629_react/exam/src/App.jsx
+++ 230629_react/exam/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
-
-function Button(props){
+// size 속성을 만들어서
+// font-size도 매개변수로 받아 적용해보세요
+function Button({name, color, size}){
   return(
-    <button style={{color:props.color}}>{props.name}</button>
+    <button style={{color:color,fontSize:size}}>{name}</button>
   )
 }
 
@@ -11,9 +12,9 @@ function App() {
     <>      
       <h1>안녕하세요</h1>  
       <p>리액트 입문</p>   
-      <Button name="1번버튼" color="red"/>
-      <Button name="2번버튼" color="green"/>
-      <Button name="3번버튼" color="gray"/>
+      <Button name="1번버튼" color="red" size="16px"/>
+      <Button name="2번버튼" color="green" size="18px"/>
+      <Button name="3번버튼" color="gray" size="24px"/>
     </>
   )
 }
