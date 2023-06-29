52218098eb64eab23668fe7b7417adeefb32f494
ingkejin <ingkejin@gmail.com>
2023-06-29 16:44:52 +0900

    레이아웃 컴포넌트

--- 230629_react/exam/src/App.jsx
+++ 230629_react/exam/src/App.jsx
@@ -1,23 +1,17 @@
 import './App.css'
 import Button from './components/Button.jsx'
-
-// components/Heading.jsx
-// export default function Heading()
-// h1 태그
-// title 속성을 받아서 화면에 렌더링
-// import Heading from './components/Heading.jsx'
+import Heading from './components/Heading'
+import Flex from './components/Flex'
 
 function App() { 
   return (    
     <>      
-      <Heading title="안녕하세요"/>
-      <Button name="1번버튼" color="red" size="16px"/>
-      <Button name="2번버튼" color="green" size="18px"/>
-      <Button name="3번버튼" color="gray" size="24px"/>
-      <Button name="3번버튼" color="gray" size="24px"/>
-      <Button name="3번버튼" color="gray" size="24px"/>
-      <Button name="3번버튼" color="gray" size="24px"/>
-      <Button name="3번버튼" color="gray" size="24px"/>
+      <Flex>       
+        <Heading title="안녕하세요"/>
+        <Button name="1번버튼" color="red" size="16px"/>
+        <Button name="2번버튼" color="green" size="18px"/>
+        <Button name="3번버튼" color="gray" size="24px"/>
+      </Flex>
     </>
   )
 }
