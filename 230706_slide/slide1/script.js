c3859538d19a2486364fe333fd1f1f7a03c0e019
ingkejin <ingkejin@gmail.com>
2023-07-07 14:11:20 +0900

    ✨ click 이벤트 추가하는 방법

--- 230706_slide/slide1/script.js
+++ 230706_slide/slide1/script.js
@@ -9,6 +9,16 @@ btn1.addEventListener('click', function(){
   slideList.style.transform = 'translateX(0)'
 })
 
+// click 이벤트 추가하는 방법
+// 1. HTML onclick="함수()"
+// 2. 함수 생성 후, 이벤트대상.addEventListner('click', 함수)
+// 3. 이벤트대상.addEventListner('click', fuction(){}) 콜백
+
+// function 버튼1(){
+//   slideList.style.transform = 'translateX(0)'
+// }
+// btn1.addEventListener('click', 버튼1)
+
 // 버튼 2 클릭 했을 때 이미지 (1) translateX()
 btn2.addEventListener('click', function(){
   slideList.style.transform = 'translateX(-100vw)'
