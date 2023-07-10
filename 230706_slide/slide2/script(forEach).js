b40aa297e97aacfcbe2b314d7fec9be9f59008f2
ingkejin <ingkejin@gmail.com>
2023-07-10 13:35:45 +0900

    ✨ 조건문으로 button active 클래스 추가/제거

--- 230706_slide/slide2/script(forEach).js
+++ 230706_slide/slide2/script(forEach).js
@@ -8,10 +8,20 @@ buttons.forEach(function(btn, index){
     
     // buttons라는 배열을 한 번 더 돌면서 모든 버튼이
     // 가지고 있는 active class remove
-    buttons.forEach(function(otherBtn){
-      otherBtn.classList.remove('active')
-    })
-    // 클릭된 버튼에만 active class add
+    // buttons.forEach(function(otherBtn){
+    //   otherBtn.classList.remove('active')
+    // })
+    // // 클릭된 버튼에만 active class add
     btn.classList.add('active');
+
+    // 조건문
+    // 현재 클릭되어 있는 버튼이 아닐 때 && 
+    // active라는 class를 포함하고 있다면
+    // 위 조건이 true일 때 active class remove
+    buttons.forEach(function(otherBtn){
+      if(otherBtn !== btn && otherBtn.classList.contains('active')){
+        otherBtn.classList.remove('active');
+      } 
+    })
   })
 })
