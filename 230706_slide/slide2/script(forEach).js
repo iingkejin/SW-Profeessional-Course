f2cbea24e9dcd5545d2eb3f4bd45b346df9d4357
ingkejin <ingkejin@gmail.com>
2023-07-10 13:12:31 +0900

    💄 button 태그 active 클래스 추가

--- 230706_slide/slide2/script(forEach).js
+++ 230706_slide/slide2/script(forEach).js
@@ -1,23 +1,17 @@
-// forEach, parameter(매개변수) 
-// forEach method : Array(배열)에서 사용한 메서드
-// 배열 안의 값을 순차적으로 함수에게 전달
-// 숫자 = [1,2,3,4,5]
-// 숫자.forEach
-
-// buttons 변수에 btn-group 클래스 안에 있는 button 태그 다 저장
 let buttons = document.querySelectorAll('.btn-group button');
 
-// forEach 파라미터
-// 1. 배열을 반복한 값 (btn)
-// 2. 배열의 인덱스 값 (index)
-// 0,1,2 * 100 /  translateX 0 -100 -200
-
-// click 이벤트 추가 => 첫번째 매개변수 (btn)
 buttons.forEach(function(btn, index){
   btn.addEventListener('click', function(){
     let slideList = document.querySelector('.slide-list');
     let xValue = -index * 100 + 'vw';
-    // slideList.style.transform = 'translateX(' + xValue + ')'
     slideList.style.transform = `translateX(${xValue})`;
+    
+    // buttons라는 배열을 한 번 더 돌면서 모든 버튼이
+    // 가지고 있는 active class remove
+    buttons.forEach(function(otherBtn){
+      otherBtn.classList.remove('active')
+    })
+    // 클릭된 버튼에만 active class add
+    btn.classList.add('active');
   })
 })
