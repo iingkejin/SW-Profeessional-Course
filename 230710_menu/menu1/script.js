88ba0224c3e43dfb9856c2531f1d77eb2b1af620
ingkejin <ingkejin@gmail.com>
2023-07-11 13:46:04 +0900

    ✨ Add stop( )

--- 230710_menu/menu1/script.js
+++ 230710_menu/menu1/script.js
@@ -1,12 +1,12 @@
 // .gnb li (메인메뉴)에 hover 했을 때
 $('.gnb > li').hover(function(){
   // this : .gnb > li들 중에 hover된 애
-  $(this).find('ul').slideDown(500);
+  $(this).find('ul').stop().slideDown(500);
   $(this).find('a').addClass('on');
 // .gnb li (메인메뉴)에 hover되지 않았을 때
 // 마우스가 떠났을 때 (mouseleave)
 // 콜백함수 : 앞에 함수가 실행되고, 또 다른 명령을 수행
 }, function(){
-  $(this).find('ul').slideUp(500);
+  $(this).find('ul').stop().slideUp(500);
   $(this).find('a').removeClass('on');
 })
