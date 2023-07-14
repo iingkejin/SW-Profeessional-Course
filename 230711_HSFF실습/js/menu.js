7e10955c2566cc3ae562a8e5ce7b9c0244dcdd3b
ingkejin <ingkejin@gmail.com>
2023-07-14 13:15:09 +0900

    ✨ menu Vanilla JS로 구현

--- 230711_HSFF실습/js/menu.js
+++ 230711_HSFF실습/js/menu.js
@@ -17,6 +17,16 @@ gnbItems.forEach(function(item){
   let subMenu = item.querySelector('.sub');
 
   item.addEventListener('mouseover', function(){
+    // sub 메뉴가 보이게 max-height : 265px;
     subMenu.classList.add('active');
+    // gnb-main-item a 태그에 on class 추가시 
+    // 메인메뉴 바뀐 컬러 유지(hover)
+    this.querySelector('a').classList.add('on');
+  })
+
+  item.addEventListener('mouseleave', function(){
+    // sub 메뉴가 보이지 않게 max-heigt : 0
+    subMenu.classList.remove('active');
+    this.querySelector('a').classList.remove('on');
   })
 })
