a17531663013c28c56415d92d44ab2f6b5775568
ingkejin <ingkejin@gmail.com>
2023-07-17 13:33:48 +0900

    ✨ .submenu-wrapper slideUp & Down

--- 230710_menu/menu2/js/script.js
+++ 230710_menu/menu2/js/script.js
@@ -1,4 +1,11 @@
 // find() : 자식 및 하위 태그 찾을 때
 // children() : 자식 요소만 찾을 때
-
 // index / eq()
+
+$('.main-menu li, .submenu-wrapper').hover(function(){
+  // hover시 실행 할 함수
+  $('.submenu-wrapper').stop().slideDown(500);
+}, function(){
+  // leave 됐을 때 실행 할 함수
+  $('.submenu-wrapper').stop().slideUp(500);
+})
