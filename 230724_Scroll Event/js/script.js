e19c2b2b063c29b62b180dac6e74511d939cd7ab
ingkejin <ingkejin@gmail.com>
2023-07-24 14:48:24 +0900

    ✨ window 스크롤 이벤트 추가

--- 230724_Scroll Event/js/script.js
+++ 230724_Scroll Event/js/script.js
@@ -22,14 +22,31 @@ window.addEventListener('scroll', function(){
 })
 
 // 스크롤이 끝까지 됐을 때 alert창으로 '스크롤 끝!'
-document.querySelector('.lorem').addEventListener('scroll', function(){
-  // scrollTop, clientHeight, scrollHeight
-  // scrollTop : 스크롤된 양 
-  let 스크롤양 = document.querySelector('.lorem').scrollTop;
-  // clientHeight : 박스가 화면에 보이는 높이값
-  let 높이 = document.querySelector('.lorem').clientHeight;
-  // 박스에 스타일로 지정된 높이값이 아니라 스크롤 가능한 높이값
-  let 스크롤높이 = document.querySelector('.lorem').scrollHeight;
-  console.log(스크롤양, 높이, 스크롤높이)
-  
+// document.querySelector('.lorem').addEventListener('scroll', function(){
+//   // scrollTop, clientHeight, scrollHeight
+//   // scrollTop : 스크롤된 양 
+//   let 스크롤양 = document.querySelector('.lorem').scrollTop;
+//   // clientHeight : 박스가 화면에 보이는 높이값
+//   let 높이 = document.querySelector('.lorem').clientHeight;
+//   // 박스에 스타일로 지정된 높이값이 아니라 스크롤 가능한 높이값
+//   let 스크롤높이 = document.querySelector('.lorem').scrollHeight;
+//   console.log(스크롤양 + 높이, 스크롤높이)
+
+//   if(스크롤양 + 높이 == 스크롤높이) {
+//     alert('스크롤 끝!');
+//   }
+// })
+
+
+// window 스크롤 다 됐을 때 alert창 '전체 스크롤 완료!'
+window.addEventListener('scroll', function(){
+  // documentElement == querySelector('html')
+  let 문서스크롤양 = document.documentElement.scrollTop;
+  let 문서높이 = document.querySelector('html').clientHeight;
+  let 문서스크롤높이 = document.querySelector('html').scrollHeight;
+  console.log(문서스크롤양 + 문서높이, 문서스크롤높이);
+
+  if(문서스크롤양 + 문서높이 == 문서스크롤높이) {
+    alert('전체 스크롤 완료!');
+  }
 })
