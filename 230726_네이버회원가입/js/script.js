e7799ab77bb7dd26c98914b6c76e5121e6c58637
ingkejin <ingkejin@gmail.com>
2023-07-28 13:13:55 +0900

    ✨ .total 체크시 .agree checked class add

--- 230726_네이버회원가입/js/script.js
+++ 230726_네이버회원가입/js/script.js
@@ -14,3 +14,22 @@ $('label').on('click', function(e){
     $(this).children("input[type='checkbox']").removeAttr('checked');
   }
 })
+
+
+// .total을 체크하면 .agree 올 체크
+// .total을 언체크하면 .agree 전부 언체크
+
+// .total .checkbox-img가 .checked 클래스를 가지고 있다면
+// .agree안 .checkbox-img에 checked class add
+// .agree안 input checkbox를 checked true
+
+// .total .checkbox-img가 .checked 클래스를 가지고 있지 않다면
+// .agree 안 .checkbox-img에 checked class remove
+// .agree 안 input checkbox를 checked false
+$('.total label').on('click', function(){
+  if($(this).children('.checkbox-img').hasClass('checked')) {
+    $('.agree').find('.checkbox-img').addClass('checked');
+  } else {
+
+  }
+})
