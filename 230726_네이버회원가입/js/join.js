204ac3139f1218f8760846308b06ca1604f3ac42
ingkejin <ingkejin@gmail.com>
2023-08-04 13:09:40 +0900

    ✨휴대전화 필수정보 조건문 구현

--- 230726_네이버회원가입/js/join.js
+++ 230726_네이버회원가입/js/join.js
@@ -265,9 +265,27 @@ $('.usermail input').focusout(function(){
 // .phonenum input에서 focusout 됐을 때
 // .phonenum input value length가 0이라면(조건)
 // .phonenum .warn에 text-red class '필수 정보입니다.
+$('.phonenum input').focusout(function(){
+  if($(this).val().length == 0) {
+    $('.phone .warn').html('<span class="text-red">필수 정보입니다.</span>');
+  } else {
+    $('.phone .warn').empty();
+  }
+}) 
+
+
 
 // #veribtn(인증번호 받기)를 클릭 했을 때
-// .phonenum input value에 숫자를 제외한 모든 문자를 제거하고,
+// .phonenum input value에 숫자가 아닌 모든 문자를 제거하고,
 // 제거한 값을 변수에 담아서 input에 다시 넣어준다.
 
 // .phonenum input value length가 10~11 자리가 아니라면(조건1)
+// .phonenum input value 값이 숫자가 아닌 경우(조건2)
+// 변수 두 개 선언 후 1,2번 조건을 모두 충족할 때만 값을 true로 준다.
+// 10~11자리가 맞을 때 / 값이 숫자일 경우
+
+// 두 조건이 모두 true일 때
+// 인증번호 입력칸을 활성화 시킨다.
+
+
+
