331cf62faa6dd7354556f7f9cb8eb68f5978f21c
ingkejin <ingkejin@gmail.com>
2023-08-07 13:50:17 +0900

    ✨ Vanailla JS로 비밀번호 조건문 구현

--- 230726_네이버회원가입/js/join.js
+++ 230726_네이버회원가입/js/join.js
@@ -36,3 +36,28 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
 })
 
 
+// 비밀번호
+document.querySelector('.userpw input').addEventListener('focusout', function(){
+  let userPw = this.value;
+  let pwExp = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/;
+
+  let pwWarn = document.querySelector('.userpw .warn');
+  let pwText = document.querySelector('.userpw .inputbox p');
+  let pwImg = document.querySelector('.userpw .inputbox img');
+
+  if(userPw.length == 0) {
+    pwWarn.innerHTML = essenInfo;
+    // pwText 내용 비우기
+    pwText.innerHTML = '';
+    pwImg.src = './images/m_icon_pw_step_01.png';
+  } else if(!pwExp.test(userPw)) {
+    pwWarn.innerHTML = '<span class="text-red">8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>';
+    pwText.innerHTML = '<span class="text-red">사용불가</span>';
+    pwImg.src = './images/m_icon_pw_step_10.png';
+  } else {
+    pwveri = true;
+    pwWarn.innerHTML = '';
+    pwText.innerHTML = '<span class="text-green">안전</span>';
+    pwImg.src = './images/m_icon_pw_step_04.png';
+  }
+})
