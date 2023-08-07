f3230c0e0d7ee493099a1125a476aad98f5500e0
ingkejin <ingkejin@gmail.com>
2023-08-07 13:30:44 +0900

    ✨ Vanilla JS로 아이디 조건문 구현

--- 230726_네이버회원가입/js/join.js
+++ 230726_네이버회원가입/js/join.js
@@ -13,4 +13,26 @@ document.querySelectorAll('input').forEach(function(input){
 
 let idveri = pwveri = pwchkveri = nameveri = bitrhveri = genderveri = phoneveri = addressveri = false;
 let mailveri = true;
+// Essential Infomation
+let essenInfo = '<span class="text-red">필수 정보입니다.</span>'; 
+
+
+// 아이디
+document.querySelector('.userid input').addEventListener('focusout', function(){
+  let userId = this.value;
+  let idExp= /^[a-z0-9]{5,8}$/;
+  let idWarn = document.querySelector('.userid .warn');
+
+  if(userId.length == 0) {
+    idWarn.innerHTML = essenInfo;
+  } else if(!idExp.test(userId)) {
+    // 정규식에 맞지 않을 때 = 조건이 참
+    idWarn.innerHTML = '<span class="text-red">5~8자의 영문 소문자만 사용 가능</span>';
+  } else {
+    // 위 조건에 둘 다 해당하지 않고 값을 잘 입력 했을 때
+    idveri = true;
+    idWarn.innerHTML = '<span class="text-green">멋진 아이디네여!</span>';
+  }
+})
+
 
