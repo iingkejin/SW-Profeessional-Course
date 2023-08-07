7b2ea1f3cd38de0bdf8ef2d37c988162911bbe03
ingkejin <ingkejin@gmail.com>
2023-08-07 13:12:11 +0900

    ✨ Vanilla JS로 .inputbox border-act class add & remove

---
+++ 230726_네이버회원가입/js/join.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('input').forEach(function(input){
+  input.addEventListener('focus', function(){
+    this.parentNode.classList.add('border-act');
+  })
+})
+
+document.querySelectorAll('input').forEach(function(input){
+  input.addEventListener('focusout', function(){
+    this.parentNode.classList.remove('border-act');
+  })
+})
