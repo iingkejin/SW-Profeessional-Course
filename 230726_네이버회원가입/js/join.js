9427694cacb3089dd878893f5b131ab114c66422
ingkejin <ingkejin@gmail.com>
2023-08-07 14:16:32 +0900

    ♻️ userPw 전역변수로 변경

--- 230726_네이버회원가입/js/join.js
+++ 230726_네이버회원가입/js/join.js
@@ -35,22 +35,23 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
   }
 })
 
+let userPw = document.querySelector('.userpw input');
 
 // 비밀번호
-document.querySelector('.userpw input').addEventListener('focusout', function(){
-  let userPw = this.value;
+userPw.addEventListener('focusout', function(){
+  let userPwVal = userPw.value;
   let pwExp = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/;
 
   let pwWarn = document.querySelector('.userpw .warn');
   let pwText = document.querySelector('.userpw .inputbox p');
   let pwImg = document.querySelector('.userpw .inputbox img');
 
-  if(userPw.length == 0) {
+  if(userPwVal.length == 0) {
     pwWarn.innerHTML = essenInfo;
     // pwText 내용 비우기
     pwText.innerHTML = '';
     pwImg.src = './images/m_icon_pw_step_01.png';
-  } else if(!pwExp.test(userPw)) {
+  } else if(!pwExp.test(userPwVal)) {
     pwWarn.innerHTML = '<span class="text-red">8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>';
     pwText.innerHTML = '<span class="text-red">사용불가</span>';
     pwImg.src = './images/m_icon_pw_step_10.png';
@@ -61,3 +62,20 @@ document.querySelector('.userpw input').addEventListener('focusout', function(){
     pwImg.src = './images/m_icon_pw_step_04.png';
   }
 })
+
+
+// 비밀번호 재확인
+document.querySelector('.userpw-chk input').addEventListener('focusout', function(){
+  let userpwChk = this.value;
+  let pwChkWarn = document.querySelector('.userpw-chk .warn');
+  let pwChkImg = document.querySelector('.userpw-chk .inputbox img');
+
+  if(userpwChk.length == 0) {
+    pwChkWarn.innerHTML = essenInfo;
+  } else if(userpwChk == userPw.value) {
+    pwchkveri = true;
+    pwChkWarn.innerHTML = '';
+    pwChkImg.src = './images/m_icon_pw_step_07.png';
+  }
+})
+
