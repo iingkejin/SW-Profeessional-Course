fbbecb519c479dc8f0c2f6e0d3059096e7a1ea19
ingkejin <ingkejin@gmail.com>
2023-08-07 14:32:28 +0900

    ✨ Vanilila JS로 이름 조건문 구현

--- 230726_네이버회원가입/js/join.js
+++ 230726_네이버회원가입/js/join.js
@@ -84,4 +84,19 @@ document.querySelector('.userpw-chk input').addEventListener('focusout', functio
 })
 
 
+// 이름
+document.querySelector('.username input').addEventListener('focusout', function(){
+  let userName = this.value;
+  let nameExp = /^[가-힣]{2,5}$/;
+  let nameWarn = document.querySelector('.username .warn');
+
+  if(userName.length == 0) {
+    nameWarn.innerHTML = essenInfo;
+  } else if(!nameExp.test(userName)) {
+    nameWarn.innerHTML = '<span class="text-red">2~5글자 사이의 한글로 입력하세요.</span>'
+  } else {
+    nameveri = true;
+    nameWarn.innerHTML = '';
+  }
+})
 
