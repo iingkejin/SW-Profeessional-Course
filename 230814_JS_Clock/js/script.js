96e41813e442d94f9a8f9e8bb5d7a4ea6259b024
ingkejin <ingkejin@gmail.com>
2023-08-17 13:43:28 +0900

    🐛 now, hr 전역변수 선언 / 시간에 맞춰 배경 변경, 버튼 활성화

--- 230814_JS_Clock/js/script.js
+++ 230814_JS_Clock/js/script.js
@@ -1,3 +1,6 @@
+let now = new Date();
+let hr = now.getHours();
+
 // new Date()
 // n초 마다 코드를 실행하는 setInterval
 setInterval(function(){
@@ -38,4 +41,28 @@ setInterval(function(){
   hr = String(hr).padStart(2, '0');
   min = String(min).padStart(2 , '0');
   sec = String(sec).padStart(2, '0');
+
+  document.querySelector('.phone-time').innerHTML = hr + ':' + min + ':' + sec;
 }, 1000)
+
+
+// 시간에 맞춰서 배경 이미지, 버튼 활성화
+// 1. 06-11 까지 **morning** background-image
+// 2. 12-15 까지 **afternoon** background-image
+// 3. 16-19 까지 **evening** background-image
+// 4. 20~ **night** background-image
+let btnList = document.querySelectorAll('.btn-list li');
+
+if(hr >= 6 && hr < 12) {
+  document.querySelector('.wrapper').classList.add('morning');
+  btnList[0].classList.add('on');
+} else if(hr >= 12 && hr < 16) {
+  document.querySelector('.wrapper').classList.add('afternoon');
+  btnList[1].classList.add('on');
+} else if(hr >= 16 && hr < 20) {
+  document.querySelector('.wrapper').classList.add('evening');
+  btnList[2].classList.add('on');
+} else {
+  document.querySelector('.wrapper').classList.add('night');
+  btnList[3].classList.add('on');
+}
