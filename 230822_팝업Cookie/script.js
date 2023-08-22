682be64b98c7d928e61675cd681a82ba83563c15
ingkejin <ingkejin@gmail.com>
2023-08-22 14:46:43 +0900

    ✨ 시간 밀리초로 변환

--- 230822_팝업Cookie/script.js
+++ 230822_팝업Cookie/script.js
@@ -17,3 +17,16 @@
 // 2. getCookie : 쿠키값을 가져오는 함수
 
 // localstorage라는 이용해서 구현 할 수도 있다.
+
+function setCookie(name, value, hours){
+  // 빈 문자열로 변수 초기화 => 쿠키 만료 시간 담을 변수
+  let expires = '';
+
+  // 만약에 hours라는 값이 있다면(true)
+  if(hours) {
+    // date라는 변수 안에 현재 시간을 가지는 Date 객체를 생성
+    let date = new Date();
+    // hour(시간) * 60(분) * 60(초) * 1000(밀리초)
+    date.setTime(date.getTime() + (hours * 60 * 60 * 1000) )
+  }
+}
