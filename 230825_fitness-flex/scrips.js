c90a9302306232403e9fcf4dbb25dacdd0aa8dc6
ingkejin <ingkejin@gmail.com>
2023-08-25 16:21:53 +0900

    ✨ forEach, insertAdjacentHTML로 workout length 만큼 html 생성

---
+++ 230825_fitness-flex/scrips.js
@@ -0,0 +1,29 @@
+let workout = [
+  {
+    title : '크로스 핏',
+    img : './images/img1.png'
+  },
+  {
+    title : '웨이트 훈련',
+    img : './images/img2.png'
+  },  
+  {
+    title : '요가',
+    img : './images/img3.png'
+  }
+]
+
+workout.forEach((item, i) => {
+  let cardItem = `
+    <div class="card-item">
+      <h2>${workout[i].title}</h2>
+      <div class="caption">
+        <p>Strength does not come from the physical capacity. It comes from an indomitable will. Know more</p>
+        <button type="button">Know more</button>
+      </div>
+      <img src="${workout[i].img}" alt="${workout[i].title} 이미지">
+    </div>
+  `
+
+  document.querySelector('.card-list').insertAdjacentHTML('beforeend', cardItem)
+})
