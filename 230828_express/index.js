bbbf70ea239608e9ee16d10598665702ce690e2c
ingkejin <ingkejin@gmail.com>
2023-09-06 14:40:35 +0900

    ✨ Session 관련 라이브러리 설치

--- 230828_express/index.js
+++ 230828_express/index.js
@@ -319,3 +319,25 @@ app.post('/join', function(requests, response){
 })
 
 
+// 회원가입, 로그인을 구현 할 수 있는 여러가지 방법 중
+// Session을 사용해서 기능 구현(가장 많이 사용)
+// 유저가 로그인 하면 Session ID 하나 발급
+// 서버측도 가지고 있고, 유저도 컴퓨터에 그 값을 가진다.
+// Session ID : 유저가 로그인 할 때 작성한 정보
+
+// 라이브러리 설치
+// npm install passport 
+// npm install passport-local 
+// npm install express-session
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+// app.use (미들웨어)
+// 서버와 요청 사이에 중간에 실행하고 싶은 코드가 있을 때 사용
+// passport 라이브러리 : 미들웨어 제공
+app.use(session({secret : 'secret', resave : true, saveUninitialized : false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
