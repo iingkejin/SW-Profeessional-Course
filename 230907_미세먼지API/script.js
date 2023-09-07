318c278dc9b872febc78946c55c2f77e62196029
ingkejin <ingkejin@gmail.com>
2023-09-07 17:09:25 +0900

    👽️ setInterval로 1시간 마다 updateData 함수 실행

--- 230907_미세먼지API/script.js
+++ 230907_미세먼지API/script.js
@@ -1,10 +1,11 @@
 var xhr = new XMLHttpRequest();
+
 var url = 'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureSidoLIst'; /*URL*/
 var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'rOGI%2BIvn6bqhwghISJpom2Dz1zr0tbNdR%2Blm8fl5xrLwUFt9EGQHinJBCAQhO4c3yub9o5bhWPy9AxyTKTen%2BQ%3D%3D'; /*Service Key*/
 queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* 응답 데이터 타입 설정 */
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* 한 페이지 결과 수 */
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지 번호 설정 */
-queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('경북'); /* 조회 할 데이터 시도 이름 설정*/
+queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('대전'); /* 조회 할 데이터 시도 이름 설정*/
 queryParams += '&' + encodeURIComponent('searchCondition') + '=' + encodeURIComponent('DAILY'); /* 데이터 기간 */
 
 xhr.open('GET', url + queryParams);
@@ -23,21 +24,28 @@ function updateData(){
           let items = responsData.response.body.items;
           console.log(items)
   
+          // 데이터를 담을 html 요소(div)
           let dataDisplay = document.getElementById('data');
+          
           // 가장 최근 데이터를 저장 할 변수
-          // 의도적으로 해당 변수에 아무런 데이터가 
-          // 할당되지 않았음을 나타내기 위해 null로 초기값 설정
+          // 의도적으로 해당 변수에 아무런 데이터가 할당되지 않았음을 나타내기 위해 null로 초기값 설정
           let latestData = null;
+
           for(let i = 0; i < items.length; i++) {
             let item = items[i];
   
-            if(item.cityName == '경주시') {
-              // dataTime 가장 최근 데이터 가져오기
-              // 
+            if(item.cityName == '유성구') {
+              // cityName이 '경주시'인 것 중에 dataTime 가장 최근 데이터 가져오기
+              // latestData 값이 비어 있을 경우 = True
+              // item.dataTime 값이 latestData.dataTime값보다 클 경우
+              // latestData에 값이 없다 || latestData가 최근값이 아니다 = 참
               if(!latestData || item.dataTime > latestData.dataTime ) {
-                latestData = item.dataTime;
+                // latestData에 더 큰 item 값을 넣어서 latestData에 제일 최근 데이터를 담는다.
+                latestData = item;
+                console.log(latestData)
+
                 let dataItem = document.createElement('div');
-                dataItem.innerHTML = item.cityName + '미세먼지 : ' + item.pm10Value;
+                dataItem.innerHTML = item.cityName + '미세먼지 : ' + latestData.pm10Value + latestData.dataTime;
                 dataDisplay.appendChild(dataItem);
               }
             }
@@ -53,4 +61,8 @@ function updateData(){
   xhr.send('');
 }
 
+// 처음에 한 번 함수 호출로 실행
 updateData();
+
+// updatate 함수를 3600000(1시간)마다 실행
+setInterval(updateData, 3600000)
