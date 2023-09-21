575195b0c16f67d6c3872328436d6b56c34f0cc9
ingkejin <ingkejin@gmail.com>
2023-09-21 15:17:00 +0900

    ✨ Header 부분  Nav component 완성

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-// import User from './component/User';
-// import State from './component/State';
-import Map from './component/Map';
+import Nav from './component/Header/Nav';
 
 function App() {
   return (
     <div className="App">
-      {/* <User/>
-      <State/> */}
-      <Map/>
+      <Nav/>
     </div>
   );
 }
---
+++ React/react-basic/src/component/Header/Nav.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Nav.css'
+import logo from '../applelogo.png'
+
+const Nav = () => {
+  let menuItems = [
+    {to : '/menu1', text : 'menu1'},
+    {to : '/menu2', text : 'menu2'},
+    {to : '/menu3', text : 'menu3'}
+  ]
+  return (
+    <header>
+      <nav>
+        <h1 className='logo'>
+          <a href="">
+            <img src={logo} alt="logo" />
+          </a>
+        </h1>
+
+        <ul className='gnb-list'>
+          {
+            menuItems.map((item, index) => {
+              return (
+                <li className='gnb-itme' key={index}>
+                  <a href={item.to}>{item.text}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
