3ffb0e2f582f55a7e84a383cc5dfeb7510cda525
ingkejin <ingkejin@gmail.com>
2023-09-22 16:42:19 +0900

    ✨ 글삭제 기능 구현 완료

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import CommetList from './component/CommetList';
-import List from './component/example/List';
 
 function App() {
   return (
     <div className="App">
-      {/* <List/> */}
       <CommetList/>
     </div>
   );
