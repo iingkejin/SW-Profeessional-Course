0f9b6f44fe62ee4ec67ea76fba4a34fb4ed91bd4
ingkejin <ingkejin@gmail.com>
2023-09-22 14:40:33 +0900

    💄 Commnet component styling

--- React/react-basic/src/component/Comment.jsx
+++ React/react-basic/src/component/Comment.jsx
@@ -3,17 +3,32 @@ let styles = {
   wrapper : {
     display : 'flex',
     justifyContent : 'space-between',
+    alignItems : 'center',
+    border : '1px solid #ccc',
+    borderRadius : '16px',
+    padding : '0 20px'
   },
+
+  img : {
+    width : '50px',
+    height : '50px',
+    borderRadius : '50%'
+  },
+
+  contentBox : {
+    flexGrow : 1,
+    marginLeft : '16px'
+  }
 }
 
 const Comment = () => {
   return (
     <div style={styles.wrapper}>
-      <div >
-        <img src="https://cdn.icon-icons.com/icons2/1161/PNG/512/1487716857-user_81635.png" alt="" />
+      <div>
+        <img style={styles.img} src="https://cdn.icon-icons.com/icons2/1161/PNG/512/1487716857-user_81635.png" alt="" />
       </div>
 
-      <div>
+      <div style={styles.contentBox}>
         <h3>User name</h3>
         <p>Content</p>
       </div>
