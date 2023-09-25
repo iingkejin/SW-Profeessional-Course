e03831649b3207dbabf04887c19ad7a44720efc9
ingkejin <ingkejin@gmail.com>
2023-09-25 16:10:05 +0900

    🚧 컴퓨터 랜덤값 선택 기능 구현

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -23,6 +23,32 @@ function App() {
   let [comSelect, setComSelect] = useState(null);
   let [result, setResult] = useState('');
 
+  // 유저랑 컴퓨터가 선택한 값 업데이트
+  let play = () => {
+
+  }
+
+  // 컴퓨터 랜덤 값 선택
+  let randomChoice = () => {
+    // choice object 키값을 배열로 만들어주는 함수 사용
+    // keys() : 객체의 key 값을 뽑아서 Array로 만들어주는 함수
+    let itemArr = Object.keys(choice);
+    
+    // 0부터 2사이에 있는 랜덤 값
+    // Math.floor 소수점 이하를 버리는 함수
+    let randomItem = Math.floor(Math.random() * itemArr.length)
+    let final = itemArr[randomItem]
+
+    // 해당 함수가 호출 됐을 때 반환할 값
+    return choice[final]
+  }
+  randomChoice()
+
+  // 조건문으로 유저, 컴퓨터 중 누가 이겼는지 판단
+  let judgment = () => {
+
+  }
+
   return (
     <div className="App">
       <div className='box-list'>
