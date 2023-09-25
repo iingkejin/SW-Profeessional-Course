0bc64f4f534480a3c3c6f4b5fb98b195436888c7
ingkejin <ingkejin@gmail.com>
2023-09-25 14:15:36 +0900

    ✨ 가위바위보 마크업 완료

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -5,7 +5,16 @@ import Box from './component/Box';
 function App() {
   return (
     <div className="App">
-      <Box/>
+      <div className='box-list'>
+        <Box title="You"/>
+        <Box title="Computer"/>
+      </div>
+
+      <div className="btn-list">
+        <button type='button'>가위</button>
+        <button type='button'>바위</button>
+        <button type='button'>보</button>
+      </div>
     </div>
   );
 }
--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Box.css'
 
-const Box = () => {
+const Box = (props) => {
   return (
     <div>
-      
+      <h1>{props.title}</h1>
+      <h2>Rock</h2>
+      <img src="https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day.jpg" alt="바위 이미지" />
+      <h2>win</h2>
     </div>
   );
 };
