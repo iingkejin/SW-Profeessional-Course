b06e1fa138f02c4b63082e5a88aa8688fc45dbfd
ingkejin <ingkejin@gmail.com>
2023-09-26 14:34:29 +0900

    💡 주석 설명 추가 및 수정

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Box from './component/Box';
 import { useState } from 'react';
@@ -23,37 +22,38 @@ function App() {
   let [comSelect, setComSelect] = useState(null);
   let [result, setResult] = useState('');
 
-  // 유저랑 컴퓨터가 선택한 값 업데이트
+  // 01. 컴퓨터 랜덤 값 선택
+  let randomChoice = () => {
+    // choice object 키값(rock, paper, scissors)을 배열로 만들어주는 함수 사용
+    // Object.keys() : 객체의 key 값을 뽑아서 Array로 만들어주는 함수
+    let itemArr = Object.keys(choice);
+    
+    // Math.floor 소수점 이하를 버리는 함수
+    // 0부터 2사이 숫자 중 랜덤 선택
+    let randomItem = Math.floor(Math.random() * itemArr.length)
+    let final = itemArr[randomItem]
+
+    // 해당 함수가 호출 됐을 때 반환할 값 
+    return choice[final]
+  }
+
+
+  // 02. 유저랑 컴퓨터가 선택한 값 업데이트
   let play = (userChoice) => {
     setUserSelect(choice[userChoice])
 
     let comChoice = randomChoice();
     setComSelect(comChoice);
 
+    // ** 결과 state 변경 함수에 judgment 를 호출해서 유저 선택값 & 컴퓨터 선택값을 넘겨준다.
     setResult(judgment(choice[userChoice], comChoice))
   }
 
-  // 컴퓨터 랜덤 값 선택
-  let randomChoice = () => {
-    // choice object 키값을 배열로 만들어주는 함수 사용
-    // keys() : 객체의 key 값을 뽑아서 Array로 만들어주는 함수
-    let itemArr = Object.keys(choice);
-    
-    // 0부터 2사이에 있는 랜덤 값
-    // Math.floor 소수점 이하를 버리는 함수
-    let randomItem = Math.floor(Math.random() * itemArr.length)
-    let final = itemArr[randomItem]
 
-    // 해당 함수가 호출 됐을 때 반환할 값
-    return choice[final]
-  }
-  randomChoice()
-
-  // 조건문으로 유저, 컴퓨터 중 누가 이겼는지 판단
+  // 03. 유저랑 컴퓨터가 선택한 값으로 누가 이겼는지 판단(조건문)
+  // ** play 함수에서 넘겨준 유저, 컴퓨터 선택값을 매개변수(user, computer)로 받는다.
   let judgment = (user, computer) => {
-    // user가 이겼는지, computer 이겼는지
-    // 어떤 값을 이용해서 판단할건지 결정
-    // 사용자가 rock 컴퓨터가 scissors 사용자 win, com lose
+    // 사용자가 rock 컴퓨터가 scissors 선택시 / 사용자 : win, computer : lose
     // 사용자가 선택한 값, 컴퓨터가 선택한 값 비교
     if(user.name == computer.name) {
       return 'tie'
@@ -65,11 +65,13 @@ function App() {
   return (
     <div className="App">
       <div className='box-list'>
+        {/* 자식 컴포넌트 Box에 props로 값 전달(title, select, result) */}
         <Box title="You" select={userSelect} result={result}/>
         <Box title="Computer" select={comSelect} result={result}/>
       </div>
 
       <div className="btn-list">
+        {/* 각 버튼을 클릭 했을 때 play 함수에 매개변수로 choice 키 값 전달 */}
         <button onClick={() => play('scissors')} type='button'>가위</button>
         <button onClick={() => play('rock')} type='button'>바위</button>
         <button onClick={() => play('paper')} type='button'>보</button>
--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './Box.css'
 
+// 부모 컴포넌트(App.js)에서 보낸 값 props로 받기
 const Box = (props) => {
+  // result라는 변수에 부모 컴포넌트에서 받은 props.result값을
+  // title이 Computer일 경우 반전시켜서 서로 반대값을 가지도록 만든다.
   let result;
 
+  // title이 Computer고, 결과가 tie가 아니고, 결과가 빈값이 아닐때
+  // 여기서 result는 변수가 아니라 props.result 값을 의미
   if(
     props.title == "Computer" &&
     props.result !== 'tie' &&
@@ -15,14 +19,21 @@ const Box = (props) => {
   }
 
   return (
+    // result 변수가 가지고 있는 win, lose, tie 값을 css 클래스 이름으로 사용
+    // 각 결과에 따라 border color 지정 backtick으로 감싸서 `문자열와 ${}`변수 사용
     <div className={`box-item ${result}`}>
       <h1>{props.title}</h1>
       {/*
-        null error 해결 방법 => 조건부 렌더링 
-        null이 아닐때만 값이 렌더링 되도록 
+        *** 조건부 렌더링
+        유저가 btn-list 안 버튼을 클릭 했을 때 useState 값이 변경되면서 select.name를 
+        읽어야 하는데, 초기값이 null이라 값을 읽지 못하므로 Error 발생!
+
+        ** 리액트는 컴포넌트는 UI를 렌더링 할 때, 보여주려는 값이 없다면 에러를 일으킨다.
+           props.select값이 있다면 우항을 렌더링, 없다면 false를 리턴해서 렌더링이 되지 않는다.
       */}
       <h2>{props.select && props.select.name}</h2>
       <img src={props.select && props.select.img} alt={props.select && props.select.name}/>
+      {/* props.result 값이 아니라 변수 result값을 바인딩 */}
       <h2>{result}</h2>
     </div>
   );
