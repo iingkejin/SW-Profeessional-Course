493cef8a669de487086d4c629a1f3b0faee6002a
ingkejin <ingkejin@gmail.com>
2023-09-26 13:24:06 +0900

    🚧 user & computer 선택값 바인딩

--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -5,7 +5,11 @@ const Box = (props) => {
   return (
     <div className='box-item'>
       <h1>{props.title}</h1>
-      <h2>Rock</h2>
+      {/*
+        null error 해결 방법 => 조건부 렌더링 
+        null이 아닐때만 값이 렌더링 되도록 
+      */}
+      <h2>{props.select && props.select.name}</h2>
       <img src="https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day.jpg" alt="바위 이미지" />
       <h2>win</h2>
     </div>
